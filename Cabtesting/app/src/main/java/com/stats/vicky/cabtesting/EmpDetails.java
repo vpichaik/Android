30885c8234cd8dfa20cc04d7bc679bbb83521e40
vignesh Kumar <vigneshp@statspade.com>
2017-11-15 21:20:13 +0530

    newfileadded

--- Cabtesting/app/src/main/java/com/stats/vicky/cabtesting/EmpDetails.java
+++ Cabtesting/app/src/main/java/com/stats/vicky/cabtesting/EmpDetails.java
@@ -1,17 +1,35 @@
 package com.stats.vicky.cabtesting;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 
 public class EmpDetails extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mtoggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emp_details);
+        mDrawerLayout = findViewById(R.id.drawerlayout);
+        mtoggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mtoggle);
+        mtoggle.syncState();
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
             }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mtoggle.onOptionsItemSelected(item))
+        {
+        return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
+}
 
