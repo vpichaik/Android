3392ce34d6d17ee5bf4f65e1f679eb836f4eaa7f
Satheesh Kumar S <satheesheee93@gmail.com>
2017-12-03 22:16:22 +0530

    systech login

--- Cabtesting/app/src/main/java/com/stats/vicky/cabtesting/Login.java
+++ Cabtesting/app/src/main/java/com/stats/vicky/cabtesting/Login.java
@@ -1,39 +1,67 @@
 package com.stats.vicky.cabtesting;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+
 public class Login extends AppCompatActivity {
-    EditText user;
-    EditText pass;
+     EditText user;
+     EditText pass;
+     static String username;
+     static String Password;
+     static Context Cont;
+     ProgressDialog progressDialog;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        user=findViewById(R.id.username);
-        pass=findViewById(R.id.passwd);
+
+
     }
 
     public void signIn(View view) {
-       String username = user.getText().toString();
-       String Password = pass.getText().toString();
-       if(username.equals(Password))
-       {
-           Log.i(Login.this.getClass().getName(),"Successfully Logged in ");
-           Intent i=new Intent(Login.this,EmpDetails.class);
-           startActivity(i);
-       }
-       else
-       {
-           Log.i(Login.this.getClass().getName(),"Failed Logged in ");
-       }
+        user=findViewById(R.id.username);
+        pass=findViewById(R.id.passwd);
+        username = user.getText().toString();
+        Password = pass.getText().toString();
+        this.Cont=this;
+        APIConnecting AP=new APIConnecting();
+        AP.execute("LOGIN",username,Password);
 
         }
+
+
+
+    public void progress()
+    {
+        progressDialog = ProgressDialog.show(Cont,
+                "ProgressDialog",
+                "Loading...");
+    }
+
+
+
+        public void redirect(String result)
+        {
+
+                // Toast.makeText(getApplicationContext(), "success",Toast.LENGTH_LONG).show();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("username",username);
+                Intent i=new Intent(Cont,EmpDetails.class);
+                i.putExtras(bundle);
+                Cont.startActivity(i);
+
+    }
 }
+
+
+
 
--- Cabtesting/app/src/main/java/com/stats/vicky/cabtesting/StatusActivity.java
+++ Cabtesting/app/src/main/java/com/stats/vicky/cabtesting/StatusActivity.java
@@ -1,19 +1,22 @@
 package com.stats.vicky.cabtesting;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import static com.stats.vicky.cabtesting.R.id.drawerLayoutSt;
 
-public class StatusActivity extends AppCompatActivity {
+public class StatusActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mtoggle;
@@ -21,7 +24,7 @@ public class StatusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
-     Toolbar toolbar=findViewById(R.id.toolbar);
+         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout = findViewById(R.id.drawerLayoutSt);
         mtoggle=new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.open,R.string.close);
@@ -29,25 +32,39 @@ public class StatusActivity extends AppCompatActivity {
         mtoggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         View nav_header = LayoutInflater.from(this).inflate(R.layout.header_nav, null);
-        Intent i =new Intent();
         Bundle bundle = getIntent().getExtras();
-        //String stuff = bundle.getString("username");
-        String source = bundle.getString("source");
-        String dest = bundle.getString("dest");
+        String stuff = bundle.getString("username");
         String time = bundle.getString("time");
-       // ((TextView) nav_header.findViewById(R.id.textView)).setText("Welcome : "+stuff);
-
+        /*String source = bundle.getString("source");
+        String dest = bundle.getString("dest");
+        String time = bundle.getString("time");*/
+       ((TextView) nav_header.findViewById(R.id.textView)).setText("Welcome : "+stuff);
+        ((TextView) this.findViewById(R.id.textView2)).setText(time);
+        ((TextView) this.findViewById(R.id.textView3)).setText("Cab Request Submitted");
 
         navigationView.addHeaderView(nav_header);
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-
-
     }
 
     public void track(View view) {
         Intent i =new Intent(StatusActivity.this,FinalActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        /*int id = item.getItemId();
+        if (id == R.id.track_status) {
+            Intent i=new Intent(EmpDetails.this,StatusActivity.class);
+            startActivity(i);
+            return  true;
+        }
+        DrawerLayout drawer =  findViewById(R.id.drawerlayout);
+
+        drawer.closeDrawer(GravityCompat.START);*/
+
+        return true;
+
     }
 }
